6bf877c7c2f1aad58842941c771102438b867166
tubins <tubin.shaji@gmail.com>
2019-10-22 23:47:35 +0100

    Add controller to create a task

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -8,3 +8,11 @@ exports.list_all_tasks = (req, res) => {
         res.json(tasks);
     });
 };
+
+exports.create_a_task = (req, res) => {
+    var new_task = new Task(req.body);
+    new_task.save((err, task) => {
+        if (err) res.send(err);
+        res.json(task);
+    });
+};
